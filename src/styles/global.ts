b9d7241c97a0fd5d86b6b66bb5fc7bad6f06fb1e
filipesaretta <filipessm124@gmail.com>
2022-11-12 16:50:51 -0300

    Initial config

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { globalCss } from ".";
+
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0
+  },
+  body: {
+    background: '$gray100',
+
+    '-webkit-font-smoothing': 'antialiased'
+  },
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400
+  }
+})
