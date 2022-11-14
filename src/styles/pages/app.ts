1390c42c3d00cd07b75bf416f58c0f45107d1967
filipesaretta <filipessm124@gmail.com>
2022-11-14 15:34:20 -0300

    Add a container to the app for share the header between  pages

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,10 @@
+import { styled } from "@stitches/react";
+
+export const Container = styled('div', {
+  width: '100%',
+  height: '100vh',
+  backgroundColor: '$gray900',
+  display: 'flex',
+  flexDirection: 'column',
+  // justifyContent: 'center',
+})
