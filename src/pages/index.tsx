8d7b0959bf489ec99a3799d2dc92e9db4f399d4c
filipesaretta <filipessm124@gmail.com>
2022-11-15 19:03:27 -0300

    Add slider to products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,21 @@ import shirt1 from '../assets/t-shirts-to-be-deleted/Shirt-1.png'
 import shirt2 from '../assets/t-shirts-to-be-deleted/shirt-2.png'
 import shirt3 from '../assets/t-shirts-to-be-deleted/Shirt-3.png'
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 32,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} alt='' width={520} height={480} ></Image>
         <footer>
           <strong>
@@ -17,8 +28,26 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} alt='' width={520} height={480} ></Image>
+        <footer>
+          <strong>
+            Camiseta XL
+          </strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt='' width={520} height={480} ></Image>
+        <footer>
+          <strong>
+            Camiseta XL
+          </strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt='' width={520} height={480} ></Image>
         <footer>
           <strong>
             Camiseta XL
