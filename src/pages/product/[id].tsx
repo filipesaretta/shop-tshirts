ebc3ec6bee39e11778c39cbe07b7a67343945d69
filipesaretta <filipessm124@gmail.com>
2022-11-16 10:56:34 -0300

    Create visual structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,19 @@
 import { useRouter } from "next/router"
+import { ContentContainer, ImageContainer, ProductContainer } from "../../styles/pages/products"
 
 export default function Product() {
   const { query } = useRouter()
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ContentContainer>
+        <h1>Product X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, ullam. Consequatur quos delectus suscipit explicabo magni, velit dolorem earum soluta eos facilis qui distinctio perspiciatis! Amet odio sed in inventore?</p>
+        <button>Buy Now</button>
+      </ContentContainer>
+    </ProductContainer>
   )
 }
