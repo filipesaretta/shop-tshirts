c2e2fd9017b4149b1655b6f53d9ce61f53e01657
filipesaretta <filipessm124@gmail.com>
2022-11-16 10:57:07 -0300

    Visual structure products

---
+++ src/styles/pages/products.ts
@@ -0,0 +1,65 @@
+import { spawn } from "child_process";
+import { styled } from "..";
+
+export const ProductContainer = styled('main', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  alignItems: 'stretch',
+  gap: '4.5rem',
+
+  maxWidth: 1180,
+  margin: '0 auto',
+
+
+})
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 576,
+  height: 656,
+  borderRadius: 8,
+  background: 'linear-gradient(180deg, #1ea473 0%, #7465d4 100%)',
+})
+
+export const ContentContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  h1: {
+    fontSize: '$2xl',
+    fontWeight: 'bold',
+    color: '$gray300',
+  },
+
+  span: {
+    fontSize: '$xl',
+    color: '$green500',
+    marginTop: '1rem',
+  },
+
+  p: {
+    marginTop: '1rem',
+    lineHeight: 1.6,
+    color: '$gray300'
+  },
+
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    border: 'none',
+    backgroundColor: '$green500',
+    transition: 'background-color 300ms ease',
+    borderRadius: 8,
+
+    cursor: 'pointer',
+    marginTop: 'auto',
+    padding: '1.25rem',
+    fontSize: '$md',
+    fontWeight: 'bold',
+
+    color: '$gray100',
+
+    '&:hover': {
+      backgroundColor: '$green300',
+    }
+  }
+})
