8667c7796824f852392610a93376d66b680cdca2
filipesaretta <filipessm124@gmail.com>
2022-11-18 16:03:47 -0300

    Create api to make checkout sessions

---
+++ src/pages/api/createCheckoutSession.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { priceId } = req.body;
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Not allowed' })
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price Id is not set' })
+  }
+
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1
+      }
+    ],
+    success_url: `${process.env.NEXT_URL}/success`,
+    cancel_url: `${process.env.NEXT_URL}/`,
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url
+  })
+}
